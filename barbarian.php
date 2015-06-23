2df030adf863fe56c6a57b12aba7756c5320169e
Salar <santinori@asheavenue.com>
2015-06-23 19:17:07 -0400

    added select for life and thp

--- barbarian.php
+++ barbarian.php
@@ -15,16 +15,75 @@
                 <div class="life-wrap">
                     <p>Life Total</p>
                     <select>
-                      <option value="volvo">Volvo</option>
-                      <option value="saab">Saab</option>
-                      <option value="mercedes">Mercedes</option>
-                      <option value="audi">Audi</option>
+                      <option value="-20">-20</option>
+                      <option value="-19">-19</option>
+                      <option value="-18">-18</option>
+                      <option value="-17">-17</option>
+                      <option value="-16">-16</option>
+                      <option value="-15">-15</option>
+                      <option value="-14">-14</option>
+                      <option value="-13">-13</option>
+                      <option value="-12">-12</option>
+                      <option value="-11">-11</option>
+                      <option value="-10">-10</option>
+                      <option value="-9">-9</option>
+                      <option value="-8">-8</option>
+                      <option value="-7">-7</option>
+                      <option value="-6">-6</option>
+                      <option value="-5">-5</option>
+                      <option value="-4">-4</option>
+                      <option value="-3">-3</option>
+                      <option value="-2">-2</option>
+                      <option value="-1">-1</option>
+                      <option value="0">0</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+                      <option value="10">10</option>
+                      <option value="11">11</option>
+                      <option value="12">12</option>
+                      <option value="13">13</option>
+                      <option value="14">14</option>
+                      <option value="15">15</option>
+                      <option value="16">16</option>
+                      <option value="17">17</option>
+                      <option value="18">18</option>
+                      <option value="19">19</option>
+                      <option selected ="selected" value="20">20</option>
                     </select>
-                    <input class="life" type="text" name="life">
+                    
                 </div>
                 <div class="thp-wrap">
                     <p>Temp Hp</p>
-                    <input class="thp" type="text" name="thp">
+                    <select>
+                      <option value="0">--</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+                      <option value="10">10</option>
+                      <option value="11">11</option>
+                      <option value="12">12</option>
+                      <option value="13">13</option>
+                      <option value="14">14</option>
+                      <option value="15">15</option>
+                      <option value="16">16</option>
+                      <option value="17">17</option>
+                      <option value="18">18</option>
+                      <option value="19">19</option>
+                      <option value="20">20</option>
+                    </select>
                 </div>
                 
             </div>
