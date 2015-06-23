e9abdaffbf8aa1afe1603a05407d9bd50ff8e1c7
Salar <santinori@asheavenue.com>
2015-06-22 22:30:44 -0400

    viewport test

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script type="text/javascript" src="/js/app.js"></script>
         <title></title>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
         <div class="wr class-selection">
