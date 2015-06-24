e8024dbc5ab397b319db258834f5d1a8775801c6
Salar <santinori@asheavenue.com>
2015-06-24 06:48:28 -0400

    removed dice

--- barbarian.php
+++ barbarian.php
@@ -397,46 +397,6 @@
                     <p class="description">try pushing the button now</p>
                 </div>
             </div>
-            <div class="dice">
-                <div class="dot one">
-                </div>
-                <div class="dot two">
-                </div>
-                <div class="dot three">
-                </div>
-                <div class="dot four">
-                </div>
-                <div class="dot five">
-                </div>
-                <div class="dot six">
-                </div>
-                <div class="dot seven">
-                </div>
-                <div class="dot eight">
-                </div>
-                <div class="dot nine">
-                </div>
-            </div>
-            <div class="dice">
-                <div class="dot one">
-                </div>
-                <div class="dot two">
-                </div>
-                <div class="dot three">
-                </div>
-                <div class="dot four">
-                </div>
-                <div class="dot five">
-                </div>
-                <div class="dot six">
-                </div>
-                <div class="dot seven">
-                </div>
-                <div class="dot eight">
-                </div>
-                <div class="dot nine">
-                </div>
-            </div>
         </div>
     </body>
 </html>
