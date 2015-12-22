ef93d8276277be2dcb2442f639777421c5c9181a
Salar <santinori@asheavenue.com>
2015-12-22 01:33:19 -0500

    setting up spell listings

--- index.php
+++ index.php
@@ -10,5 +10,148 @@
     </head>
     <body>
         
+        <div>
+            <p>Spell List</p>
+            <ul>
+                <li>
+                    <a>Bard</a>
+                </li>
+                
+                <li>
+                    <a>Cleric</a>
+                </li>
+                
+                <li>
+                    <a>Druid</a>
+                </li>
+                
+                <li>
+                    <a>Paladin</a>
+                </li>
+                
+                <li>
+                    <a>Ranger</a>
+                </li>
+                
+                <li>
+                    <a>Sorcerer</a>
+                </li>
+                
+                <li>
+                    <a class="button">Warlock</a>
+                    <ul class="list">
+                        <li>
+                            <a class="button">Cantrips</a>
+                            <ul class="list">
+                                <li>ddd</li>
+                                <li>eee</li>
+                            </ul>
+                        </li>
+                        <li>1st Level</li>
+                    </ul>
+                </li>
+                
+                <li>
+                    <a class="button">Wizard</a>
+                    <ul class="list">
+                        <li>
+                            <a class="button">Cantrips</a>
+                            <ul class="list">
+                                <li>
+                                    <a class="button">Acid Splash</a>
+                                    <div class="list">
+                                        <p>Conjuration cantrip</p>
+                                        <p>Casting Time: 1 action</p>
+                                        <p>Range: 60 feet</p>
+                                        <p>Components: V, S</p>
+                                        <p>Duration: Instantaneous</p>
+                                        <p>You hurl a bubble of acid. Choose one creature within range, or choose two creatures within range that are within 5 feet of each other. A target must succeed on a Dexterity saving throw or take 1d6 acid damage. This spell's damage increases by 1d6 when you reach 5th level (2d6), 11th level (3d6), and 17th level (4d6).</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a class="button">Blade Ward</a>
+                                    <div class="list">
+                                        <p>Abjuration cantrip</p>
+                                        <p>Casting Time: 1 action</p>
+                                        <p>Range: Self</p>
+                                        <p>Components: V, S</p>
+                                        <p>Duration: 1 round</p>
+                                        <p>You extend your hand and trace a sigil of warding in the air. Until the end of your next turn, you have resistance against bludgeoning, piercing, and slashing damage dealt by weapon attacks.</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a class="button">Chill Touch</a>
+                                    <div class="list">
+                                        <p>Necromancy cantrip</p>
+                                        <p>Casting Time: 1 action</p>
+                                        <p>Range: 120 feet</p>
+                                        <p>Components: V, S</p>
+                                        <p>Duration: 1 round</p>
+                                        <p>You create a ghostly, skeletal hand in the space of a creature within range. Make a ranged spell attack against the creature to assail it with the chill of the grave. On a hit, the target takes 1d8 necrotic damage, and it can't regain hit points until the start of your next turn. Until then, the hand clings to the target. If you hit an undead target, it also has disadvantage on attack rolls against you until the end of your next turn. This spell's damage increases by 1d8 when you reach 5th level (2d8), 11th level (3d8), and 17th level (4d8).</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a class="button">Dancing Lights</a>
+                                    <div class="list">
+                                        <p>Evocation cantrip</p>
+                                        <p>Casting Time: 1 action</p>
+                                        <p>Range: 120 feet</p>
+                                        <p>Components: V, S, M (a bit of phosphorus or wychwood, or a glowworm)</p>
+                                        <p>Duration: Concentration, up to 1 minute</p>
+                                        <p>You create up to four torch-sized lights within range, making them appear as torches, lanterns, or glowing orbs that hover in the air for the duration. You can also combine the four lights into one glowing vaguely humanoid form of Medium size. Whichever form you choose, each light sheds dim light in a 10-foot radius. As a bonus action on your turn, you can move the lights up to 60 feet to a new spot within range. A light must be within 20 feet of another light created by this spell, and a light winks out if it exceeds the spell's range.</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a class="button">Fire Bolt</a>
+                                    <div class="list">
+                                        <p>Evocation cantrip</p>
+                                        <p>Casting Time: 1 action</p>
+                                        <p>Range: 120 feet</p>
+                                        <p>Components: V, S</p>
+                                        <p>Duration: Instantaneous</p>
+                                        <p>You hurl a mote of fire at a creature or object within range. Make a ranged spell attack against the target. On a hit, the target takes 1d10 fire damage. A flammable object hit by this spell ignites if it isn't being worn or carried. This spell's damage increases by 1d10 when you reach 5th level (2d10), 11th level (3d10), and 17th level (4d10).</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a class="button">Friends</a>
+                                </li>
+                                <li>
+                                    <a class="button">Light</a>
+                                </li>
+                                <li>
+                                    <a class="button">Mage Hand</a>
+                                </li>
+                                <li>
+                                    <a class="button">Mending</a>
+                                </li>
+                                <li>
+                                    <a class="button">Message</a>
+                                </li>
+                                <li>
+                                    <a class="button">Minor Illusion</a>
+                                </li>
+                                <li>
+                                    <a class="button">Poison Spray</a>
+                                </li>
+                                <li>
+                                    <a class="button">Prestidigitation</a>
+                                </li>
+                                <li>
+                                    <a class="button">Ray of Frost</a>
+                                </li>
+                                <li>
+                                    <a class="button">Shocking Grasp</a>
+                                </li>
+                                <li>
+                                    <a class="button">True Strike</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>1st Level</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        
     </body>
 </html>
---
+++ spells.php
@@ -0,0 +1,402 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="/css/stylesheets/screen.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script type="text/javascript" src="/js/app.js"></script>
+        <title></title>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" >
+    </head>
+    <body>
+        <div class="wr barbarian">
+            <p>Barbarian here</p>
+            <div class="life-and-thp">
+                <div class="life-wrap">
+                    <p>Life Total</p>
+                    <select>
+                      <option value="-20">-20</option>
+                      <option value="-19">-19</option>
+                      <option value="-18">-18</option>
+                      <option value="-17">-17</option>
+                      <option value="-16">-16</option>
+                      <option value="-15">-15</option>
+                      <option value="-14">-14</option>
+                      <option value="-13">-13</option>
+                      <option value="-12">-12</option>
+                      <option value="-11">-11</option>
+                      <option value="-10">-10</option>
+                      <option value="-9">-9</option>
+                      <option value="-8">-8</option>
+                      <option value="-7">-7</option>
+                      <option value="-6">-6</option>
+                      <option value="-5">-5</option>
+                      <option value="-4">-4</option>
+                      <option value="-3">-3</option>
+                      <option value="-2">-2</option>
+                      <option value="-1">-1</option>
+                      <option value="0">0</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+                      <option value="10">10</option>
+                      <option value="11">11</option>
+                      <option value="12">12</option>
+                      <option value="13">13</option>
+                      <option value="14">14</option>
+                      <option value="15">15</option>
+                      <option value="16">16</option>
+                      <option value="17">17</option>
+                      <option value="18">18</option>
+                      <option value="19">19</option>
+                      <option selected ="selected" value="20">20</option>
+                    </select>
+                    
+                </div>
+                <div class="thp-wrap">
+                    <p>Temp Hp</p>
+                    <select>
+                      <option value="0">--</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+                      <option value="10">10</option>
+                      <option value="11">11</option>
+                      <option value="12">12</option>
+                      <option value="13">13</option>
+                      <option value="14">14</option>
+                      <option value="15">15</option>
+                      <option value="16">16</option>
+                      <option value="17">17</option>
+                      <option value="18">18</option>
+                      <option value="19">19</option>
+                      <option value="20">20</option>
+                    </select>
+                </div>
+                
+            </div>
+            <div class="clr"></div>
+            
+            <div class="scores">
+                <div class="as-wrap">
+                    <div class="as strength">
+                        <p class="as-desc">Strength</p>
+                        <p class="as-val">9 <span>(+2)</span></p>
+                    </div>
+                    <div class="as dexterity">
+                        <p class="as-desc">Dexterity</p>
+                        <p class="as-val">12 <span>(+3)</span></p>
+                    </div>
+                    <div class="as constitution">
+                        <p class="as-desc">Constitution</p>
+                        <p class="as-val">6 <span>(0)</span></p>
+                    </div>
+                    <div class="as intelligence">
+                        <p class="as-desc">Intelligence</p>
+                        <p class="as-val">8 <span>(+1)</span></p>
+                    </div>
+                    <div class="as wisdom">
+                        <p class="as-desc">Wisdom</p>
+                        <p class="as-val">11 <span>(+2)</span></p>
+                    </div>
+                    <div class="as charisma">
+                        <p class="as-desc">Charisma</p>
+                        <p class="as-val">14 <span>(+3)</span></p>
+                    </div>
+                    <div class="clr"></div>
+                </div>
+                <div class="ss-wrap">
+                    <div class="ss">
+                        <p class="ss-desc">Acrobatics<span>5</span></p>
+                    </div>
+                    <div class="ss shade">
+                        <p class="ss-desc">Animal Handling<span>7</span></p>
+                    </div>
+                    <div class="ss">
+                        <p class="ss-desc">Arcana<span>2</span></p>
+                    </div>
+                    <div class="ss shade">
+                        <p class="ss-desc">Athletics<span>9</span></p>
+                    </div>
+                    <div class="ss">
+                        <p class="ss-desc">Deception<span>3</span></p>
+                    </div>
+                    <div class="ss shade">
+                        <p class="ss-desc">History<span>4</span></p>
+                    </div>
+                    <div class="ss">
+                        <p class="ss-desc">Insight<span>5</span></p>
+                    </div>
+                    <div class="ss shade">
+                        <p class="ss-desc">Intimidation<span>6</span></p>
+                    </div>
+                    <div class="ss">
+                        <p class="ss-desc">Investigation<span>2</span></p>
+                    </div>
+                </div>
+                <div class="ss-wrap">
+                    <div class="ss shade">
+                        <p class="ss-desc">Medicine<span>6</span></p>
+                    </div>
+                    <div class="ss">
+                        <p class="ss-desc">Nature<span>8</span></p>
+                    </div>
+                    <div class="ss shade">
+                        <p class="ss-desc">Perception<span>3</span></p>
+                    </div>
+                    <div class="ss">
+                        <p class="ss-desc">Performance<span>5</span></p>
+                    </div>
+                    <div class="ss shade">
+                        <p class="ss-desc">Persuasion<span>6</span></p>
+                    </div>
+                    <div class="ss">
+                        <p class="ss-desc">Religion<span>5</span></p>
+                    </div>
+                    <div class="ss shade">
+                        <p class="ss-desc">Sleight of Hand<span>4</span></p>
+                    </div>
+                    <div class="ss">
+                        <p class="ss-desc">Stealth<span>7</span></p>
+                    </div>
+                    <div class="ss shade">
+                        <p class="ss-desc">Survival<span>9</span></p>
+                    </div>
+                </div>
+                <div class="stat-wrap">
+                    <div class="stat arm">
+                        <p class="stat-desc">
+                            Armor Class
+                        </p>
+                        <p class="stat-val">
+                            25
+                        </p>
+                    </div>
+                    <div class="stat spe">
+                        <p class="stat-desc">
+                            Speed
+                        </p>
+                        <p class="stat-val">
+                            8
+                        </p>
+                    </div>
+                    <div class="stat ini">
+                        <p class="stat-desc">
+                            Initiative
+                        </p>
+                        <p class="stat-val">
+                            +6
+                        </p>
+                    </div>
+                    <div class="stat pro">
+                        <p class="stat-desc">
+                            Proficiency Bonus
+                        </p>
+                        <p class="stat-val">
+                            +2
+                        </p>
+                    </div>
+                </div>
+                <div class="clr"></div>
+            </div>
+            
+            <div class="aic-button">
+                <div class="aic-page one">
+                    <p class="aic-label">Combat</p>
+                </div>
+                <div class="aic-page two">
+                    <p class="aic-label">Spells</p>
+                </div>
+                <div class="aic-page three">
+                    <p class="aic-label">Features</p>
+                </div>
+                <div class="aic-page four">
+                    <p class="aic-label">Class/Race</p>
+                </div>
+            </div>
+            <div class="combat">
+                <div class="attack">
+                    <p class="action">Attack</p>
+                    <p class="description">talk abou attacks</p>
+                </div>
+                <div class="cast-a-spell">
+                    <p class="action">Cast A Spell</p>
+                    <p class="description">shoot some magic</p>
+                </div>
+                <div class="dash">
+                    <p class="action">Dash</p>
+                    <p class="description">quick get out of there</p>
+                </div>
+                <div class="disengage">
+                    <p class="action">Disengage</p>
+                    <p class="description">staying safe never hurts</p>
+                </div>
+                <div class="dodge">
+                    <p class="action">Dodge</p>
+                    <p class="description">like a shadowy ninja</p>
+                </div>
+                <div class="help">
+                    <p class="action">Help</p>
+                    <p class="description">help your friends get high</p>
+                </div>
+                <div class="hide">
+                    <p class="action">Hide</p>
+                    <p class="description">even shadowyer ninja</p>
+                </div>
+                <div class="ready">
+                    <p class="action">Ready</p>
+                    <p class="description">costs a response action</p>
+                </div>
+                <div class="search">
+                    <p class="action">Search</p>
+                    <p class="description">maybe there is a button</p>
+                </div>
+                <div class="use-an-object">
+                    <p class="action">Use An Object</p>
+                    <p class="description">try pushing the button now</p>
+                </div>
+            </div>
+            
+            <div class="spells hide">
+                <div class="attack">
+                    <p class="action">2nd page</p>
+                    <p class="description">talk abou attacks</p>
+                </div>
+                <div class="cast-a-spell">
+                    <p class="action">Cast A Spell</p>
+                    <p class="description">shoot some magic</p>
+                </div>
+                <div class="dash">
+                    <p class="action">Dash</p>
+                    <p class="description">quick get out of there</p>
+                </div>
+                <div class="disengage">
+                    <p class="action">Disengage</p>
+                    <p class="description">staying safe never hurts</p>
+                </div>
+                <div class="dodge">
+                    <p class="action">Dodge</p>
+                    <p class="description">like a shadowy ninja</p>
+                </div>
+                <div class="help">
+                    <p class="action">Help</p>
+                    <p class="description">help your friends get high</p>
+                </div>
+                <div class="hide">
+                    <p class="action">Hide</p>
+                    <p class="description">even shadowyer ninja</p>
+                </div>
+                <div class="ready">
+                    <p class="action">Ready</p>
+                    <p class="description">costs a response action</p>
+                </div>
+                <div class="search">
+                    <p class="action">Search</p>
+                    <p class="description">maybe there is a button</p>
+                </div>
+                <div class="use-an-object">
+                    <p class="action">Use An Object</p>
+                    <p class="description">try pushing the button now</p>
+                </div>
+            </div>
+            
+            <div class="features hide">
+                <div class="attack">
+                    <p class="action">3rd page</p>
+                    <p class="description">talk abou attacks</p>
+                </div>
+                <div class="cast-a-spell">
+                    <p class="action">Cast A Spell</p>
+                    <p class="description">shoot some magic</p>
+                </div>
+                <div class="dash">
+                    <p class="action">Dash</p>
+                    <p class="description">quick get out of there</p>
+                </div>
+                <div class="disengage">
+                    <p class="action">Disengage</p>
+                    <p class="description">staying safe never hurts</p>
+                </div>
+                <div class="dodge">
+                    <p class="action">Dodge</p>
+                    <p class="description">like a shadowy ninja</p>
+                </div>
+                <div class="help">
+                    <p class="action">Help</p>
+                    <p class="description">help your friends get high</p>
+                </div>
+                <div class="hide">
+                    <p class="action">Hide</p>
+                    <p class="description">even shadowyer ninja</p>
+                </div>
+                <div class="ready">
+                    <p class="action">Ready</p>
+                    <p class="description">costs a response action</p>
+                </div>
+                <div class="search">
+                    <p class="action">Search</p>
+                    <p class="description">maybe there is a button</p>
+                </div>
+                <div class="use-an-object">
+                    <p class="action">Use An Object</p>
+                    <p class="description">try pushing the button now</p>
+                </div>
+            </div>
+            
+            <div class="class-race hide">
+                <div class="attack">
+                    <p class="action">4th</p>
+                    <p class="description">talk abou attacks</p>
+                </div>
+                <div class="cast-a-spell">
+                    <p class="action">Cast A Spell</p>
+                    <p class="description">shoot some magic</p>
+                </div>
+                <div class="dash">
+                    <p class="action">Dash</p>
+                    <p class="description">quick get out of there</p>
+                </div>
+                <div class="disengage">
+                    <p class="action">Disengage</p>
+                    <p class="description">staying safe never hurts</p>
+                </div>
+                <div class="dodge">
+                    <p class="action">Dodge</p>
+                    <p class="description">like a shadowy ninja</p>
+                </div>
+                <div class="help">
+                    <p class="action">Help</p>
+                    <p class="description">help your friends get high</p>
+                </div>
+                <div class="hide">
+                    <p class="action">Hide</p>
+                    <p class="description">even shadowyer ninja</p>
+                </div>
+                <div class="ready">
+                    <p class="action">Ready</p>
+                    <p class="description">costs a response action</p>
+                </div>
+                <div class="search">
+                    <p class="action">Search</p>
+                    <p class="description">maybe there is a button</p>
+                </div>
+                <div class="use-an-object">
+                    <p class="action">Use An Object</p>
+                    <p class="description">try pushing the button now</p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
