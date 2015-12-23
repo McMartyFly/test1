625118bccfab816ae775efc7c14f0cba36e2caca
Salar <santinori@asheavenue.com>
2015-12-22 21:16:00 -0500

    cool block style to wiz

--- index.php
+++ index.php
@@ -10,146 +10,136 @@
     </head>
     <body>
         
-        <div>
+        <div class="spell-list">
             <a></a>
             <p>Spell List</p>
             <ul>
                 <li>
-                    <a>Bard</a>
+                    <div class="button indent-0">Bard</div>
                 </li>
                 
                 <li>
-                    <a>Cleric</a>
+                    <div class="button indent-0">Cleric</div>
                 </li>
                 
                 <li>
-                    <a>Druid</a>
+                    <div class="button indent-0">Druid</div>
                 </li>
                 
                 <li>
-                    <a>Paladin</a>
+                    <div class="button indent-0">Paladin</div>
                 </li>
                 
                 <li>
-                    <a>Ranger</a>
+                    <div class="button indent-0">Ranger</div>
                 </li>
                 
                 <li>
-                    <a>Sorcerer</a>
+                    <div class="button indent-0">Sorcerer</div>
                 </li>
                 
                 <li>
-                    <a class="button indent-0">Warlock</a>
+                    <div class="button indent-0">Warlock</div>
+                </li>
+                
+                <li class="wiz">
+                    <div class="button indent-0 wiz">Wizard</div>
                     <ul class="list">
                         <li>
-                            <a class="button indent-1">Cantrips</a>
-                            <ul class="list">
-                                <li>ddd</li>
-                                <li>eee</li>
-                            </ul>
-                        </li>
-                        <li>1st Level</li>
-                    </ul>
-                </li>
-                
-                <li>
-                    <a class="button indent-0">Wizard</a>
-                    <ul class="list">
-                        <li>
-                            <a class="button indent-1">Cantrips</a>
+                            <div class="button indent-1 wiz">Cantrips</div>
                             <ul class="list">
                                 <li>
-                                    <a class="button indent-2">Acid Splash</a>
+                                    <div class="button indent-2">Acid Splash</div>
                                     <div class="list">
-                                        <p>Conjuration cantrip</p>
-                                        <p>Casting Time: 1 action</p>
-                                        <p>Range: 60 feet</p>
-                                        <p>Components: V, S</p>
-                                        <p>Duration: Instantaneous</p>
-                                        <p>You hurl a bubble of acid. Choose one creature within range, or choose two creatures within range that are within 5 feet of each other. A target must succeed on a Dexterity saving throw or take 1d6 acid damage.<br />&#160;&#160;This spell's damage increases by 1d6 when you reach 5th level (2d6), 11th level (3d6), and 17th level (4d6).</p>
+                                        <div>Conjuration cantrip</div>
+                                        <div>Casting Time: 1 action</div>
+                                        <div>Range: 60 feet</div>
+                                        <div>Components: V, S</div>
+                                        <div>Duration: Instantaneous</div>
+                                        <div>You hurl a bubble of acid. Choose one creature within range, or choose two creatures within range that are within 5 feet of each other. A target must succeed on a Dexterity saving throw or take 1d6 acid damage.<br />&#160;&#160;This spell's damage increases by 1d6 when you reach 5th level (2d6), 11th level (3d6), and 17th level (4d6).</div>
                                     </div>
                                 </li>
                                 <li>
-                                    <a class="button indent-2">Blade Ward</a>
+                                    <div class="button indent-2">Blade Ward</div>
                                     <div class="list">
-                                        <p>Abjuration cantrip</p>
-                                        <p>Casting Time: 1 action</p>
-                                        <p>Range: Self</p>
-                                        <p>Components: V, S</p>
-                                        <p>Duration: 1 round</p>
-                                        <p>You extend your hand and trace a sigil of warding in the air. Until the end of your next turn, you have resistance against bludgeoning, piercing, and slashing damage dealt by weapon attacks.</p>
+                                        <div>Abjuration cantrip</div>
+                                        <div>Casting Time: 1 action</div>
+                                        <div>Range: Self</div>
+                                        <div>Components: V, S</div>
+                                        <div>Duration: 1 round</div>
+                                        <div>You extend your hand and trace a sigil of warding in the air. Until the end of your next turn, you have resistance against bludgeoning, piercing, and slashing damage dealt by weapon attacks.</div>
                                     </div>
                                 </li>
                                 <li>
-                                    <a class="button indent-2">Chill Touch</a>
+                                    <div class="button indent-2">Chill Touch</div>
                                     <div class="list">
-                                        <p>Necromancy cantrip</p>
-                                        <p>Casting Time: 1 action</p>
-                                        <p>Range: 120 feet</p>
-                                        <p>Components: V, S</p>
-                                        <p>Duration: 1 round</p>
-                                        <p>You create a ghostly, skeletal hand in the space of a creature within range. Make a ranged spell attack against the creature to assail it with the chill of the grave. On a hit, the target takes 1d8 necrotic damage, and it can't regain hit points until the start of your next turn. Until then, the hand clings to the target.<br />&#160;&#160;If you hit an undead target, it also has disadvantage on attack rolls against you until the end of your next turn.<br />&#160;&#160;This spell's damage increases by 1d8 when you reach 5th level (2d8), 11th level (3d8), and 17th level (4d8).</p>
+                                        <div>Necromancy cantrip</div>
+                                        <div>Casting Time: 1 action</div>
+                                        <div>Range: 120 feet</div>
+                                        <div>Components: V, S</div>
+                                        <div>Duration: 1 round</div>
+                                        <div>You create a ghostly, skeletal hand in the space of a creature within range. Make a ranged spell attack against the creature to assail it with the chill of the grave. On a hit, the target takes 1d8 necrotic damage, and it can't regain hit points until the start of your next turn. Until then, the hand clings to the target.<br />&#160;&#160;If you hit an undead target, it also has disadvantage on attack rolls against you until the end of your next turn.<br />&#160;&#160;This spell's damage increases by 1d8 when you reach 5th level (2d8), 11th level (3d8), and 17th level (4d8).</div>
                                     </div>
                                 </li>
                                 <li>
-                                    <a class="button indent-2">Dancing Lights</a>
+                                    <div class="button indent-2">Dancing Lights</div>
                                     <div class="list">
-                                        <p>Evocation cantrip</p>
-                                        <p>Casting Time: 1 action</p>
-                                        <p>Range: 120 feet</p>
-                                        <p>Components: V, S, M (a bit of phosphorus or wychwood, or a glowworm)</p>
-                                        <p>Duration: Concentration, up to 1 minute</p>
-                                        <p>You create up to four torch-sized lights within range, making them appear as torches, lanterns, or glowing orbs that hover in the air for the duration. You can also combine the four lights into one glowing vaguely humanoid form of Medium size. Whichever form you choose, each light sheds dim light in a 10-foot radius.<br />&#160;&#160;As a bonus action on your turn, you can move the lights up to 60 feet to a new spot within range. A light must be within 20 feet of another light created by this spell, and a light winks out if it exceeds the spell's range.</p>
+                                        <div>Evocation cantrip</div>
+                                        <div>Casting Time: 1 action</div>
+                                        <div>Range: 120 feet</div>
+                                        <div>Components: V, S, M (a bit of phosphorus or wychwood, or a glowworm)</div>
+                                        <div>Duration: Concentration, up to 1 minute</div>
+                                        <div>You create up to four torch-sized lights within range, making them appear as torches, lanterns, or glowing orbs that hover in the air for the duration. You can also combine the four lights into one glowing vaguely humanoid form of Medium size. Whichever form you choose, each light sheds dim light in a 10-foot radius.<br />&#160;&#160;As a bonus action on your turn, you can move the lights up to 60 feet to a new spot within range. A light must be within 20 feet of another light created by this spell, and a light winks out if it exceeds the spell's range.</div>
                                     </div>
                                 </li>
                                 <li>
-                                    <a class="button indent-2">Fire Bolt</a>
+                                    <div class="button indent-2">Fire Bolt</div>
                                     <div class="list">
-                                        <p>Evocation cantrip</p>
-                                        <p>Casting Time: 1 action</p>
-                                        <p>Range: 120 feet</p>
-                                        <p>Components: V, S</p>
-                                        <p>Duration: Instantaneous</p>
-                                        <p>You hurl a mote of fire at a creature or object within range. Make a ranged spell attack against the target. On a hit, the target takes 1d10 fire damage. A flammable object hit by this spell ignites if it isn't being worn or carried.<br />&#160;&#160;This spell's damage increases by 1d10 when you reach 5th level (2d10), 11th level (3d10), and 17th level (4d10).</p>
+                                        <div>Evocation cantrip</div>
+                                        <div>Casting Time: 1 action</div>
+                                        <div>Range: 120 feet</div>
+                                        <div>Components: V, S</div>
+                                        <div>Duration: Instantaneous</div>
+                                        <div>You hurl a mote of fire at a creature or object within range. Make a ranged spell attack against the target. On a hit, the target takes 1d10 fire damage. A flammable object hit by this spell ignites if it isn't being worn or carried.<br />&#160;&#160;This spell's damage increases by 1d10 when you reach 5th level (2d10), 11th level (3d10), and 17th level (4d10).</div>
                                     </div>
                                 </li>
                                 <li>
-                                    <a class="button">Friends</a>
+                                    <div class="button indent-2">Friends</div>
                                 </li>
                                 <li>
-                                    <a class="button">Light</a>
+                                    <div class="button indent-2">Light</div>
                                 </li>
                                 <li>
-                                    <a class="button">Mage Hand</a>
+                                    <div class="button indent-2">Mage Hand</div>
                                 </li>
                                 <li>
-                                    <a class="button">Mending</a>
+                                    <div class="button indent-2">Mending</div>
                                 </li>
                                 <li>
-                                    <a class="button">Message</a>
+                                    <div class="button indent-2">Message</div>
                                 </li>
                                 <li>
-                                    <a class="button">Minor Illusion</a>
+                                    <div class="button indent-2">Minor Illusion</div>
                                 </li>
                                 <li>
-                                    <a class="button">Poison Spray</a>
+                                    <div class="button indent-2">Poison Spray</div>
                                 </li>
                                 <li>
-                                    <a class="button">Prestidigitation</a>
+                                    <div class="button indent-2">Prestidigitation</div>
                                 </li>
                                 <li>
-                                    <a class="button">Ray of Frost</a>
+                                    <div class="button indent-2">Ray of Frost</div>
                                 </li>
                                 <li>
-                                    <a class="button">Shocking Grasp</a>
+                                    <div class="button indent-2">Shocking Grasp</div>
                                 </li>
                                 <li>
-                                    <a class="button">True Strike</a>
+                                    <div class="button indent-2">True Strike</div>
                                 </li>
                             </ul>
                         </li>
                         <li>
-                            <a class="button indent-1">1st Level</a>
+                            <div class="button indent-1 wiz">1st Level</div>
                         </li>
                     </ul>
                 </li>
