e4dbc4c34989c4d28c5933e21de95d0388a35b0b
Salar <santinori@asheavenue.com>
2016-01-19 00:56:39 -0500

    trying more stuff

--- index.php
+++ index.php
@@ -5,9 +5,8 @@
         <link rel="stylesheet" type="text/css" href="/slick/slick-theme.css"/>
         <link rel="stylesheet" type="text/css" href="/css/stylesheets/screen.css"/>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-        <script type="text/javascript" src="/js/app.js"></script>
         <script type="text/javascript" src="/slick/slick.js"></script>
-        
+        <script type="text/javascript" src="/js/app.js"></script>
         
         
         <title></title>
